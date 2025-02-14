316f4f3e1959784e0beba3a21354bbc5829191c0
Zaheen <mdzaheen90@gmail.com>
2025-02-15 02:23:27 +0530

    modified header and hero

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -3,12 +3,23 @@ import "../public/styles/hero.css";
 
 const HeroSection = () => {
   return (
-    <section className="hero">
-      <h2>Protecting Your Rights In The Field Of Law & Order</h2>
-      <div className="hero-images">
-       
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Protecting Your Rights In The Field Of Law & Order</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Massa scelerisque enim dui tristique massa hendrerit.
+          Phasellus est aliquet suspendisse sed et cras viverra vitae. Ipsum in tempor turpis ut aliquet tincidunt.
+        </p>
+        <button className="hero-button">BOOK APPOINTMENT →</button>
       </div>
-    </section>
+      <div className="image-container">
+        <img src="/images/image1.png" alt="Law Image 1" className="image1" />
+        <img src="/images/image2.png" alt="Law Image 2" className="image2" />
+        <img src="/images/image3.png" alt="Law Image 3" className="image3" />
+        <img src="/images/image4.png" alt="Law Image 4" className="image4" />
+      </div>
+    </div>
   );
 };
+
 export default HeroSection;
