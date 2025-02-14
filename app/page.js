2e10b8c358292ce6dc988d074c4d7eee64a8d8fe
arfath <arfathkhan063@gmail.com>
2025-02-15 02:23:58 +0530

    half work done

--- app/page.js
+++ app/page.js
@@ -3,8 +3,9 @@ import Header from "../components/Header.js";
 import HeroSection from "../components/HeroSection.js";
 import TrustedLawFirm from "../components/TrustedLawFirm.js";
 import Appointment from "../components/Appointment.js";
-import Footer from "../components/Footer.js";
-import Know from "@/components/know.js";
+import Know from "@/components/Know.js";
+// import Footer from "../components/Footer.js";
+
 
 
 const Page = () => {
