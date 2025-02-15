21b38f74a6f08033bdb34bd72258a3d6049b5890
MannanShariff <mannanshariff04@gmail.com>
2025-02-15 16:17:19 +0530

    Added TrustLawFirm again

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Header from "../components/Header.js";
 import HeroSection from "../components/HeroSection.js";
 import TrustedLawFirm from "../components/TrustedLawFirm.js";
 import Appointment from "../components/Appointment.js";
-import Footer from "../components/Footer.js";
+// import Footer from "../components/Footer.js";
 import Know from "../components/Know.js";
 
 
