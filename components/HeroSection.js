73c91752557f06e9e0558e1ddef9c37edb8829d5
Zaheen <mdzaheen90@gmail.com>
2025-02-20 00:17:27 +0530

    modified hero and header

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -6,11 +6,13 @@ const HeroSection = () => {
     <div className="hero-section">
       <div className="hero-content">
         <h1 className="lawhead">Protecting Your Rights In The Field Of Law & Order</h1>
+        <div className="paragra">
         <p>
           Lorem ipsum dolor sit amet consectetur. Massa scelerisque enim dui tristique massa hendrerit.
           Phasellus est aliquet suspendisse sed et cras viverra vitae. Ipsum in tempor turpis ut aliquet tincidunt.
         </p>
         <button className="hero-button">BOOK APPOINTMENT →</button>
+        </div>
       </div>
       <div className="image-container">
         <img src="/images/image1.png" alt="Law Image 1" className="imageone" />
